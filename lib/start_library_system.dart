f3ef3752e4408475fdfd5debb0bdb0afc8481296
MiltonDJC <miltonaxe2001@gmail.com>
2025-10-01 08:50:37 -0300

    Translate startLibrarySystem method

--- lib/start_library_system.dart
+++ lib/start_library_system.dart
@@ -1,36 +1,36 @@
 import 'package:library_system/models/library.dart';
-import 'package:library_system/models/libro.dart';
-import 'package:library_system/models/usuario.dart';
-import 'package:library_system/services/prestamo_service.dart';
+import 'package:library_system/models/book.dart';
+import 'package:library_system/models/user.dart';
+import 'package:library_system/services/circulation_service.dart';
 
 void startLibrarySystem() {
-  //Creación de libros:
-  Libro libro1 = Libro('Cien años de soledad', 'Gabriel García');
-  Libro libro2 = Libro('1984', 'George Orwell');
-  Libro libro3 = Libro('El señor de los Anillos', 'J. R. R. Tolkien');
+  //Books created:
+  Book book1 = Book('Cien años de soledad', 'Gabriel García');
+  Book book2 = Book('1984', 'George Orwell');
+  Book book3 = Book('El señor de los Anillos', 'J. R. R. Tolkien');
 
-  //Creación de Usuarios:
-  Usuario usuario1 = Usuario('Barbara');
+  //Users created:
+  User user1 = User('Barbara');
 
-  //Creación de Biblioteca:
-  Biblioteca biblioteca = Biblioteca();
+  //Library created:
+  Library library = Library();
 
-  //Establecer formato digital para el primer y último libro:
-  libro1.formatoDigital = 'PDF';
-  libro3.formatoDigital = 'ePub';
+  //Set digital format for the first and last book:
+  book1.digitalFormat = 'PDF';
+  book3.digitalFormat = 'ePub';
 
-  //Usuario presta un libro:
-  PrestamoService.prestamoService.prestarLibro(usuario1, libro1);
-  //Manejo de error controlado:
-  PrestamoService.prestamoService.prestarLibro(usuario1, libro2);
-  //Usuario devuelve un libro:
-  PrestamoService.prestamoService.devolverLibro(usuario1, libro1);
-  //Manejo de error controlado:
-  PrestamoService.prestamoService.devolverLibro(usuario1, libro2);
+  //User borrow a book:
+  CirculationService.circulationService.lendBook(user1, book1);
+  //Controlled error handling:
+  CirculationService.circulationService.lendBook(user1, book2);
+  //User return a book:
+  CirculationService.circulationService.returnBook(user1, book1);
+  //Controlled error handling:
+  CirculationService.circulationService.returnBook(user1, book2);
 
-  biblioteca.agregarProducto(libro1);
-  biblioteca.agregarProducto(libro2);
-  biblioteca.agregarProducto(libro3);
+  library.addProduct(book1);
+  library.addProduct(book2);
+  library.addProduct(book3);
 
-  biblioteca.mostrarProductos();
+  library.showProducts();
 }
