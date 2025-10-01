4be4140ca12f3376ddaaecf8412fb1cde3a078ac
MiltonDJC <miltonaxe2001@gmail.com>
2025-09-30 22:00:19 -0300

    Update Usuario class

--- lib/models/usuario.dart
+++ lib/models/usuario.dart
@@ -4,31 +4,4 @@ class Usuario {
   String nombre;
   List<Libro> librosPrestados = [];
   Usuario(this.nombre);
-  void prestarLibro(Libro libro) {
-    if (isLibroPrestable(libro)) {
-      librosPrestados.add(libro);
-      libro.disponible = false;
-      print(libroPrestadoMessage(libro.disponible));
-    }
-  }
-
-  void devolverLibro(Libro libro) {
-    if (librosPrestados.contains(libro)) {
-      librosPrestados.remove(libro);
-      libro.disponible = true;
-      print(libroDevueltoMessage(libro.disponible));
-    } else {
-      print(libroDevueltoMessage(libro.disponible));
-    }
-  }
-
-  bool isLibroPrestable(Libro libro) => libro.disponible ? true : false;
-
-  String libroPrestadoMessage(bool disponible) => disponible
-      ? 'Libro prestado al usuario: $nombre.'
-      : 'El libro no puede ser prestado nuevamente.';
-
-  String libroDevueltoMessage(bool disponible) => disponible
-      ? 'Libro devuelto exitosamente.'
-      : 'Libro no adquirido previamente.';
 }
